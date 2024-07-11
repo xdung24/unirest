8de48561fe17121f9d3dcbab0ac89b23e78bc186
Dung Le <xdung24@gmail.com>
2024-07-11 11:44:26 +0700

    Added build time

--- build.sh
+++ build.sh
@@ -15,9 +15,11 @@ echo "GO_ARCH: $GO_ARCH"
 echo "GO_VERSION: $GO_VERSION"
 echo "APP_VERSION: $APP_VERSION"
 echo "GIT_HASH: $GIT_HASH"
+echo "BUILDTIME: $BUILDTIME"
 echo "GIN_VERSION: $GIN_VERSION"
 echo "GORILLA_VERSION: $GORILLA_VERSION"
 echo "ECHO_VERSION: $ECHO_VERSION"
 echo "FIBER_VERSION: $FIBER_VERSION"
 
-CGO_ENABLED=0 GOOS=$GO_OS GOARCH=$GO_ARCH go build -o ./tmp/main.exe -x --ldflags="-X 'main.GoOs=$GO_OS' -X 'main.GoArch=$GO_ARCH' -X 'main.GoVersion=$GO_VERSION' -X 'main.AppVersion=$APP_VERSION' -X 'main.GitHash=$GIT_HASH' -X 'main.GinVersion=$GIN_VERSION' -X 'main.GorillaVersion=$GORILLA_VERSION' -X 'main.EchoVersion=$ECHO_VERSION' -X 'main.FiberVersion=$FIBER_VERSION'" | true
+ldflags="-X 'main.GoOs=$GO_OS' -X 'main.GoArch=$GO_ARCH' -X 'main.GoVersion=$GO_VERSION' -X 'main.AppVersion=$APP_VERSION' -X 'main.GitHash=$GIT_HASH' -X 'main.BuildTime=$BUILDTIME' -X 'main.GinVersion=$GIN_VERSION' -X 'main.GorillaVersion=$GORILLA_VERSION' -X 'main.EchoVersion=$ECHO_VERSION' -X 'main.FiberVersion=$FIBER_VERSION'"
+CGO_ENABLED=0 GOOS=$GO_OS GOARCH=$GO_ARCH go build -o ./tmp/main.exe -x --ldflags=$ldflags | true
